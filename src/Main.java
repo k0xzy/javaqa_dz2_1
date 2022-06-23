ebf6c402787db293ee07b6b9daaa8c849634b611
k0xzy <koxzy22@gmail.com>
2022-06-23 13:49:40 +0300

    dz1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 1000;
+        int bonus = price / 20;
+        System.out.println("Количество бонусных миль = " + bonus);
+    }
+}
